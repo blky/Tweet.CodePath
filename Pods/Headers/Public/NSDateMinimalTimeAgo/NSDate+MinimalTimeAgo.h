3dce5d4e284d61974d13379a2ade0e09cd4351b2
blky <jademails@hotmail.com>
2014-10-10 16:39:15 -0700

    gitignore

--- Pods/Headers/Public/NSDateMinimalTimeAgo/NSDate+MinimalTimeAgo.h
+++
@@ -1,1 +0,0 @@
-../../../NSDateMinimalTimeAgo/Classes/NSDate+MinimalTimeAgo.h
--- Pods/Target Support Files/Pods-NSDateMinimalTimeAgo/Pods-NSDateMinimalTimeAgo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NSDateMinimalTimeAgo : NSObject
-@end
-@implementation PodsDummy_Pods_NSDateMinimalTimeAgo
-@end
